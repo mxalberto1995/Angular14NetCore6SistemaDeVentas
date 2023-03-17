164d17097d647170485ec1d10dc8e219ceb03246
mxalberto1995 <maxwellchaconnica@gmail.com>
2023-03-17 08:48:43 -0600

    Actualizando Angular 15

--- SistemaVentaAngular/SistemaVentaAngular/Program.cs
+++ SistemaVentaAngular/SistemaVentaAngular/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddSwaggerGen();
 
 
 builder.Services.AddDbContext<DBVentaAngularContext>(options =>
@@ -29,6 +30,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseStaticFiles();
